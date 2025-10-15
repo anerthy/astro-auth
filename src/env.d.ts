37d8ecd58d24d950dc5e8d5ff8169fb98d282a19
anerthy <112020687+anerthy@users.noreply.github.com>
2025-10-14 20:55:24 -0600

    feat: add environment variable handling for Firebase configuration and remove hardcoded values

--- src/env.d.ts
+++ src/env.d.ts
@@ -12,3 +12,17 @@ declare namespace App {
     user: User | null;
   }
 }
+
+interface ImportMetaEnv {
+  readonly WEBSITE_URL: string;
+  readonly FIREBASE_API_KEY: string;
+  readonly FIREBASE_AUTH_DOMAIN: string;
+  readonly FIREBASE_PROJECT_ID: string;
+  readonly FIREBASE_STORAGE_BUCKET: string;
+  readonly FIREBASE_MESSAGING_SENDER_ID: string;
+  readonly FIREBASE_APP_ID: string;
+  readonly FIREBASE_MEASUREMENT_ID: string;
+}
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,15 +1,22 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
+if (!import.meta.env.FIREBASE_API_KEY ) throw new Error('Missing FIREBASE_API_KEY in environment variables');
+if (!import.meta.env.FIREBASE_AUTH_DOMAIN ) throw new Error('Missing FIREBASE_AUTH_DOMAIN in environment variables');
+if (!import.meta.env.FIREBASE_PROJECT_ID ) throw new Error('Missing FIREBASE_PROJECT_ID in environment variables');
+if (!import.meta.env.FIREBASE_STORAGE_BUCKET ) throw new Error('Missing FIREBASE_STORAGE_BUCKET in environment variables');
+if (!import.meta.env.FIREBASE_MESSAGING_SENDER_ID ) throw new Error('Missing FIREBASE_MESSAGING_SENDER_ID in environment variables');
+if (!import.meta.env.FIREBASE_APP_ID ) throw new Error('Missing FIREBASE_APP_ID in environment variables');
+if (!import.meta.env.FIREBASE_MEASUREMENT_ID ) throw new Error('Missing FIREBASE_MEASUREMENT_ID in environment variables');
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCD_W_L8cXNB9fyoWlD2pdocA9K3oFEe-0",
-  authDomain: "astro-authentication-bea00.firebaseapp.com",
-  projectId: "astro-authentication-bea00",
-  storageBucket: "astro-authentication-bea00.firebasestorage.app",
-  messagingSenderId: "1039912122755",
-  appId: "1:1039912122755:web:a952f6907e5f50033c5eb6",
-  measurementId: "G-YWJGR6WZ1G"
+  apiKey: import.meta.env.FIREBASE_API_KEY,
+  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.FIREBASE_APP_ID,
+  measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
